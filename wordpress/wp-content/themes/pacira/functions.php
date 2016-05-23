bf7757b3bb5ca83959bdeb22a33ef88bab1ebe62
Andre Gordon <andre.gordon@gmail.com>
2016-05-23 17:09:58 -0400

    Paragraph Module
    began styling most paragraph text options.
    ...continue tweeking MCE Buttons 2

--- wordpress/wp-content/themes/pacira/functions.php
+++ wordpress/wp-content/themes/pacira/functions.php
@@ -166,6 +166,49 @@ function bones_theme_customizer($wp_customize) {
   //
   // Uncomment the below lines to remove the default customize sections 
 
+/* ============== *** CUSTOM SECTION *** ======================== */
+
+// Callback function to insert 'styleselect' into the $buttons array
+function my_mce_buttons_2( $buttons ) {
+  array_unshift( $buttons, 'styleselect' );
+  return $buttons;
+}
+// Register our callback to the appropriate filter
+add_filter( 'mce_buttons_2', 'my_mce_buttons_2' );
+
+// Callback function to filter the MCE settings
+function my_mce_before_init_insert_formats( $init_array ) {  
+  // Define the style_formats array
+  $style_formats = array(  
+    // Each array child is a format with it's own settings
+    array(  
+      'title' => '.translation',  
+      'block' => 'blockquote',  
+      'classes' => 'translation',
+      'wrapper' => true,
+    ),  
+    array(  
+      'title' => '⇠.rtl',  
+      'block' => 'blockquote',  
+      'classes' => 'rtl',
+      'wrapper' => true,
+    ),
+    array(  
+      'title' => '.ltr⇢',  
+      'block' => 'blockquote',  
+      'classes' => 'ltr',
+      'wrapper' => true,
+    ),
+  );  
+  // Insert the array, JSON ENCODED, into 'style_formats'
+  $init_array['style_formats'] = json_encode( $style_formats );  
+  
+  return $init_array;  
+  
+} 
+// Attach callback to 'tiny_mce_before_init' 
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' ); 
+
   // $wp_customize->remove_section('title_tagline');
   // $wp_customize->remove_section('colors');
   // $wp_customize->remove_section('background_image');
