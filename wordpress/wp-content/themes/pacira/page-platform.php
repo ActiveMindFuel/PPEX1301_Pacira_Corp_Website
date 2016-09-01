1c51ac859d49dfc1715c4dd25dc157d8dae00dc9
Andre Gordon <andre.gordon@gmail.com>
2016-09-01 16:48:54 -0400

    Final tweeks

--- wordpress/wp-content/themes/pacira/page-platform.php
+++ wordpress/wp-content/themes/pacira/page-platform.php
@@ -370,7 +370,7 @@
 
 		<!-- sidebar-module ************************************************************************************************************************** -->
 
-		<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3"><?php get_sidebar( 'Platform' ); ?></div>
+		<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3"><?php get_sidebar( 'platform' ); ?></div>
 	</div>
 </div>
 </div>
